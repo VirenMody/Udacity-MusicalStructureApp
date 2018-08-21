a3fd4493b816f9de86954f1ca57077c0f2218cb0
Viren Mody <virenmody@gmail.com>
2018-08-21 16:59:12 -0500

    Added NowPlayingActivity and onClickListener from MainActivity

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.android.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Retrieve the TextView for Now Playing Activity and attach an onClicklistener to it
+        TextView nowPlaying = (TextView) findViewById(R.id.now_playing);
+        nowPlaying.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                startActivity(nowPlayingIntent);
+            }
+        });
+
     }
 }
