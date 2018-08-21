daadecbf09c0b1378eea0db77862599edda70e37
Viren Mody <virenmody@gmail.com>
2018-08-21 17:02:08 -0500

    Added AllMusicShuffledActivity

---
+++ app/src/main/java/com/example/android/musicalstructureapp/AllMusicShuffledActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.musicalstructureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllMusicShuffledActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_music_shuffled);
+    }
+}
