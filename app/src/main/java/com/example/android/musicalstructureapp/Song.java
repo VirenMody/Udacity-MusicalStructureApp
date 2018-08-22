376ec7ab238c08a3a847c2a836590862226cbbe4
Viren Mody <virenmody@gmail.com>
2018-08-21 20:21:26 -0500

    added a Song class

---
+++ app/src/main/java/com/example/android/musicalstructureapp/Song.java
@@ -0,0 +1,30 @@
+package com.example.android.musicalstructureapp;
+
+/**
+ * The Song class represents a song in the user's music app.
+ * It contains 2 fields: song name and artist name
+ */
+public class Song {
+
+    private String mSongName;
+    private String mArtistName;
+
+    /**
+     * Constructor for Song objects
+     *
+     * @param songName is the name of the song
+     * @param artistName is the name of the artist who wrote/sings the song
+     */
+    public Song (String songName, String artistName) {
+        mSongName = songName;
+        mArtistName = artistName;
+    }
+
+    public String getmSongName() {
+        return mSongName;
+    }
+
+    public String getmArtistName() {
+        return mArtistName;
+    }
+}
